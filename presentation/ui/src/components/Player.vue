33d1fc8d14351f246bbcf9e566bb553a7e7736de
tfilla <tfilla.primary@gmail.com>
2022-03-11 18:32:29 -0700

    adding node server

--- presentation/ui/src/components/Player.vue
+++ presentation/ui/src/components/Player.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container class="">
     <v-card align="center">
-        <v-card-title>
-            Currently Playing: 
-        </v-card-title>
-      <v-card-body>
-          
-      </v-card-body>
+      <v-card-title> Currently Playing: </v-card-title>
+      <v-card-subtitle>
+        {{ artist }} <br>
+        {{ song }} <br>
+        {{ album }} ({{ year }})
+      </v-card-subtitle>
+      <v-card-body> </v-card-body>
     </v-card>
   </v-container>
 </template>
@@ -16,6 +17,10 @@ export default {
   name: "Player",
   data: () => ({
     url: "",
+    artist: 'slightly stoopid',
+    song: 'leaving on a jetplane',
+    album: 'everything you need',
+    year: 2003
   }),
 };
 </script>
