5b8fcbd0b942e070a6cd4bec14a714c4ab87e22c
tfilla <tfilla.primary@gmail.com>
2022-03-11 17:36:41 -0700

    update theme, add player

---
+++ presentation/ui/src/components/Player.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="">
+    <v-card align="center">
+        <v-card-title>
+            Currently Playing: 
+        </v-card-title>
+      <v-card-body>
+          
+      </v-card-body>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Player",
+  data: () => ({
+    url: "",
+  }),
+};
+</script>
+
+<style scoped>
+</style>
